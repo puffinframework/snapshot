5e9ce1a8b5c1d4adf6197ed4d23f3135c00ba7bc
dfreire <dario.freire@gmail.com>
2014-12-07 20:48:30 +0000

    update

--- snapshot.go
+++ snapshot.go
@@ -3,10 +3,15 @@ package snapshot
 import (
 	"errors"
 	"github.com/puffinframework/config"
+	"labix.org/v2/mgo/bson"
 )
 
 var (
-	ErrOpenDB error = errors.New("snapshot: couldnt open database")
+	ErrOpenDB            error = errors.New("snapshot: couldn't open database")
+	ErrGetSnapshot       error = errors.New("snapshot: couldn't get the snapshot from the db")
+	ErrPutSnapshot       error = errors.New("snapshot: couldn't put the snapshot from the db")
+	ErrUnmarshalSnapshot error = errors.New("snapshot: couldn't unmarshal the snapshot")
+	ErrMarshalSnapshot   error = errors.New("snapshot: couldn't marshal the snapshot")
 )
 
 type Store interface {
@@ -35,10 +40,30 @@ func NewLeveldbStore() *Store {
 	return &leveldbStore{db: db}
 }
 
-func (self *leveldbStore) LoadSnapshot(key string, snapshot interface{}) {
+func (self *leveldbStore) LoadSnapshot(key string, snapshot interface{}) error {
+	value, err := self.db.Get([]byte(key), nil)
+	if err != nil {
+		return ErrGetSnapshot
+	}
+
+	if err = bson.Unmarshal(value, snapshot); err != nil {
+		return ErrUnmarshalSnapshot
+	}
+
+	return nil
 }
 
-func (self *leveldbStore) SaveSnapshot(key string, snapshot interface{}) {
+func (self *leveldbStore) SaveSnapshot(key string, snapshot interface{}) error {
+	value, err := bson.Marshal(snapshot)
+	if err != nil {
+		return ErrMarshalSnapshot
+	}
+
+	if err = db.Put([]byte(string), value, nil); err != nil {
+		return ErrPutSnapshot
+	}
+
+	return nil
 }
 
 func (self *leveldbStore) Close() {
